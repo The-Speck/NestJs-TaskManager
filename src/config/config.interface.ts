64b39a8328e47b6b9094ebf6097d96c0b25ddc9a
Alex <a.plyushchay@gmail.com>
2019-12-10 22:14:40 -0500

    remove unnecessary type in config interface

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,6 +1,6 @@
 import { IDatabaseOptions } from '../database/database.interface';
 
-export type TEnvironments = 'development' | 'production' | 'test' | 'uat';
+export type TEnvironments = 'development' | 'production' | 'test';
 
 export interface IConfigOptions extends IDatabaseOptions {
   APP_ENV: string;
