75e510cab29d110aa6070a870b83b656113c1960
Alex <a.plyushchay@gmail.com>
2019-12-11 23:03:43 -0500

    make jwt options configurable via env settings

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -4,4 +4,6 @@ export type TEnvironments = 'development' | 'production' | 'test';
 
 export interface ConfigOptions extends DatabaseOptions {
   APP_ENV: string;
+  JWT_SECRET: string;
+  JWT_EXPIRATION: number;
 }
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,31 +1,33 @@
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
+import ConfigError from './config.error';
 import { ConfigOptions, TEnvironments } from './config.interface';
 
 export class ConfigService {
   public static readonly DEVELOPMENT: 'development' = 'development';
 
-  private readonly envConfig: ConfigOptions;
+  private readonly INTEGER_SPECIFIER = '+';
+  private readonly configOptions: ConfigOptions;
   private readonly logger = new Logger(ConfigService.name);
 
   constructor() {
     const environment: TEnvironments = this.getEnvironment();
-    this.envConfig = this.getConfigOptions(environment);
+    this.configOptions = this.getConfigOptions(environment);
 
     this.logger.log(`Starting server with ${environment} settings`);
   }
 
   public read(): ConfigOptions {
-    return this.envConfig;
+    return this.configOptions;
   }
 
   public isDev(): boolean {
-    return this.envConfig.APP_ENV === 'development';
+    return this.configOptions.APP_ENV === 'development';
   }
 
   public isProd(): boolean {
-    return this.envConfig.APP_ENV === 'production';
+    return this.configOptions.APP_ENV === 'production';
   }
 
   private getEnvironment(): TEnvironments {
@@ -34,15 +36,38 @@ export class ConfigService {
 
   private getConfigOptions(environment: TEnvironments): ConfigOptions {
     const data: any = dotenv.parse(fs.readFileSync(`${environment}.env`));
-    return this.populateConfigOptions(data, environment);
+    this.validateConfigOptions(data, environment);
+    return data;
   }
 
-  private populateConfigOptions(
-    data: any,
-    environment: TEnvironments,
-  ): ConfigOptions {
+  private validateConfigOptions(data: any, environment: TEnvironments): void {
+    this.setEnv(data, environment);
+    this.setInts(data);
+  }
+
+  private setEnv(data: any, environment: TEnvironments): void {
     data.APP_ENV = environment;
-    data.DB_PORT = parseInt(data.DB_PORT, 10);
-    return data;
+  }
+
+  private setInts(data: any): void {
+    for (const option in data) {
+      if (data[option].startsWith(this.INTEGER_SPECIFIER)) {
+        const value = Number(data[option].substring(1));
+        this.validateNumber(option, value);
+
+        data[option] = value;
+      }
+    }
+  }
+
+  private validateNumber(key: string, value: number): boolean {
+    if (isNaN(value)) {
+      throw new ConfigError(
+        `Non-Integer config options should not start with '${
+          this.INTEGER_SPECIFIER
+        }'. The Following property is invalid ${key}`,
+      );
+    }
+    return true;
   }
 }
