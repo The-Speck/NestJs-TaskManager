1d0f4e1a5a3e9cab6a23aaa9a983b5e610ba6881
Alex <a.plyushchay@gmail.com>
2019-12-11 22:02:59 -0500

    create Utils class and add quickUUID

---
+++ src/Utils.ts
@@ -0,0 +1,13 @@
+export default class Utils {
+  static quickUUID(uniqueness: number = 2): string {
+    const subs = [];
+    for (let i = 0; i < uniqueness; i++) {
+      subs.push(
+        Math.random()
+          .toString(36)
+          .substring(2, 15),
+      );
+    }
+    return subs.join('');
+  }
+}
